d06f2375907434b2efaab94112e3f74b645d015e
Rohit D <rodiwaa@gmail.com>
2017-12-05 22:50:20 +0530

    Set endtimer when starting timer on click

--- src/common/components/Start.js
+++ src/common/components/Start.js
@@ -1,11 +1,13 @@
 import React from 'React'
+import moment from 'moment'
 import { View } from 'react-native'
 import { Container, Header, Left, Body, Title, Button, Content, List, ListItem, Text, Right, Icon } from 'native-base'
 
 export default class Start extends React.Component {
   state = {
     hour: '4',
-    minute: '30'
+    minute: '30',
+    isRunning: false
   }
 
   componentWillMount() {
@@ -18,6 +20,49 @@ export default class Start extends React.Component {
     }
   }
 
+  initiateCountdown() {
+    if (this.state.isRunning) {
+      this.stopTimer()
+    } else {
+      this.startTimer()
+    }
+  }
+
+  stopTimer() {
+    this.setState({ isRunning: false })
+  }
+
+  startTimer() {
+    this.setState({ isRunning: true })
+    this.setEndTime()
+    this.startCountdownTimer()
+  }
+
+  setEndTime() {
+    const { hour, minute } = this.props.navigation.state.params.time
+    const startTimeinHM = moment()
+    const endTimeinHM = moment(startTimeinHM).add({ hours: hour, minutes: minute })
+    console.log(`Added duration for: ${hour}:${minute}`)
+    console.log(moment(startTimeinHM).format('h:m'))
+    console.log(moment(endTimeinHM).format('h:m'))
+    // call action to set endTime in firebase
+    /*
+      payload: {
+        startTime
+        endTime,
+        isRnning: true
+      }
+    */
+  }
+
+  startCountdownTimer() {
+
+  }
+
+  getDuration () {
+    return 100
+  }
+
   render () {
     return (
       <Container>
@@ -25,15 +70,16 @@ export default class Start extends React.Component {
           <View style={styles.view}>
             <Text style={styles.tim}>
               { `${this.state.hour}:${this.state.minute}` }
-            </Text>  
+            </Text>
+            <Text> { this.state.isRunning ? 'Timer is running' : '' } </Text>
           </View>
           <Button 
             block
             success
             style={styles.button}
-            onPress={()=>this.props.navigation.navigate('Time')}
+            onPress={()=>this.initiateCountdown()}
             >
-            <Text>Start</Text>
+            <Text>{ this.state.isRunning ? 'Stop' : 'Start' }</Text>
           </Button>
         </Content>
       </Container>
